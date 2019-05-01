2cbdfd344ab0dcbdc520990f9e1970ff449d34a0
Brian McCann <39921556+Prometheus66@users.noreply.github.com>
2019-05-01 18:03:47 -0400

    Update lab7assembly.s

--- lab7assembly.s
+++ lab7assembly.s
@@ -1,11 +1,12 @@
 	.data
-board:	.string 0xC, "|---------------------------------------------|", 0xA, 0xD, "|*********************************************|", 0xA, 0xD, "|*****     *****     *****     *****     *****|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|.............................................|", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|&............................................|", 0xA, 0xD, "|---------------------------------------------|", 0
+board:	.string 0xC, "|---------------------------------------------|", 0xA, 0xD, "|*********************************************|", 0xA, 0xD, "|*****     *****     *****     *****     *****|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|", 0xA, 0xD, "|.............................................|", 0xA, 0xD, "|                           C                 |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "| C                                           |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|               C                             |", 0xA, 0xD, "|                                             |", 0xA, 0xD, "|&............................................|", 0xA, 0xD, "|---------------------------------------------|", 0
 legend:	.string "| |: Vertical Wall      | +10 for moving forward one space   |", 0xA, 0xD, "| -: Horizontal Wall    | -10 for moving back one space      |", 0xA, 0xD, "| a: Alligator's Back   | +50 for getting a frog home safely |", 0xA, 0xD, "| A: Alligator's Mouth  | +100 for eating a fly              |", 0xA, 0xD, "| L: Log                | +250 for completing the level      |", 0xA, 0xD, "| O: Lily Pad           | +10 for each second of unused time |", 0xA, 0xD, "| &: Frog               |	 when getting a frog home    |", 0xA, 0xD, "| T: Turtle             |", 0xA, 0xD, "| C: Car                |", 0xA, 0xD, "| #: Truck              |", 0xA, 0xD, "| +: Fly                |", 0xA, 0xD, "| H: Occupied Home      |", 0xA, 0xD, "| ~: Water              |", 0
 
 
 	.text
 	.global lab7
 	.global Timer0Handler
+	.global Timer1Handler
 	.global Uart0Handler
 	.global PortAHandler
 	.global uart_init
@@ -29,11 +30,11 @@ replayprompt: .string "Press q to quit, or press anything else to play again.", 
 startprompt: .string "Press space to start the game, then W, A, S, or D to move.  To pause/resume, press any button on the keypad.", 0
 explainprompt: .string "Traverse through the board, without hitting any cars, trucks, or water, and get your frog home.", 0
 
-Timer0Handler:
+Timer0Handler: ; This is the timer for the board/frog from movement
 	STMFD sp!,{lr}
-
 ;------------setting r11 to the location of the next astrics spot-------------
 	STRB v2, [r11]
+	ADD v7, v7, #1
 
 	CMP v5, #0x77	; w
 	BNE checks
@@ -65,6 +66,13 @@ homecheck:
 
 	CMP v2, #0x7E	; compare to ~
 	BEQ deadfrog
+	CMP v2, #0x41	; compare to A
+	BEQ deadfrog
+	CMP v2, #0x23	; compare to #
+	BEQ deadfrog
+	CMP v2, #0x43	; compare to C
+	BEQ deadfrog
+
 
 	MOV a3, #0x0090
 	MOVT a3, #0x2000
@@ -157,8 +165,7 @@ notminus:
 	MOV v1, #0x26
 	STRB v1, [r11]	; store & to new location
 boardout:
-	LDR r4, ptr
-	BL output_string	; print the board to putty
+
 	B endtimer
 ;---------------------------------dead frog---------------------------
 deadfrog:
@@ -188,7 +195,7 @@ not2:
 	BNE endtimer
 	MOV a3, #0x0
 	STR a3, [r12]
-	B gameover
+	B gameover	; need to set a flag to read from in busy loop
 respawn:
 	STRB v2, [r11]
 
@@ -199,13 +206,85 @@ respawn:
 
 	;need to find a random spot along the bottom to place the asterisk
 	;for now, it will just go to the bottom corner
+
+
+;-------------------------end dead frog------------------------------
+
+
+;--------------------movement for logs, etc, only do this half the time-----------------------
+endtimer:
+	CMP v7, #2
+	BNE timerfinale
+	SUB v7, v7, #2
+
+;#0x2000724D
+	STMFD sp!, {r2, r3, r10, r11, r12}
+	MOV r10, #0x024E
+	MOVT r10, #0x2000
+
+	MOV r11, #0x01EC
+	MOVT r11, #0x2000
+
+	MOV r12, #0x018A
+	MOVT r12, #0x2000
+
+carloop2:
+	LDRB r3, [r10], #1
+	CMP r3, #0x20
+	BEQ carloop2
+	CMP r3, #0x7C
+	BEQ endloops	; get rid of character
+	MOV r2, #0x20
+	STRB r2, [r10, #-1]
+	STRB r3, [r10]
+
+
+
+carloop4:
+
+
+	LDRB r3, [r11], #1
+	CMP r3, #0x20
+	BEQ carloop4
+	CMP r3, #0x7C
+	BEQ endloops	; get rid of character
+	MOV r2, #0x20
+	STRB r2, [r11, #-1]
+	STRB r3, [r11]
+
+
+carloop6:
+
+	LDRB r3, [r12], #1
+	CMP r3, #0x20
+	BEQ carloop6
+	CMP r3, #0x7C
+	BEQ endloops	; get rid of character
+	MOV r2, #0x20
+	STRB r2, [r12, #-1]
+	STRB r3, [r12]
+
+
+removechar:
+
+
+endloops:
+	LDMFD sp!, {r2, r3, r10, r11, r12}
+
+
+
+;--------------------end movement for logs, etc------------------------------------------------
+timerfinale:
+
 	MOV a3, #0x26
 	STRB a3, [r11]
 
 	LDR r4, ptr
 	BL output_string	; print the board to putty
-;-------------------------end dead frog------------------------------
-endtimer:
+	MOV r9, v3
+	BL inttoascii
+	BL character_newline
+
 	MOV v5, #0x00
 	MOV r0, #0x0024		; go to TIMER0 Interrupt Clear GPTMICR at 0x40030000 offset 0x24, and clear bit 0 by writting a 1 to it
 	MOVT r0, #0x4003
@@ -213,7 +292,30 @@ endtimer:
 	ORR r2, r2, #0x1
 	STR r2, [r0]
 
+;-----------clear psr----------
+;	MOV r8, #0xFFFF
+;	MOVT r8, #0x0FFF
+;	LDR r11, [r12]
+;	AND r11, r11, r8
+;	MSR APSR_nzcvq, r11
+;---------end clear psr---------
+
 	LDMFD sp!, {lr}
+	BX lr
+
+
+Timer1Handler: ; This is the handler for the game countdown
+	STMFD sp!,{lr,r0,r2,r4,r9}
+
+	SUB v3, v3, #1
+
+	MOV r0, #0x1024		; go to TIMER0 Interrupt Clear GPTMICR at 0x40030000 offset 0x24, and clear bit 0 by writting a 1 to it
+	MOVT r0, #0x4003
+	LDR r2, [r0]
+	ORR r2, r2, #0x1
+	STR r2, [r0]
+
+	LDMFD sp!, {lr,r0,r2,r4,r9}
 	BX lr
 
 Uart0Handler:
@@ -260,7 +362,7 @@ PortAHandler:
 	BEQ resume
 	MOV r0, #0x2
 	BL illuminate_RGB_LED
-	;print a legend here
+
 	LDR r4, ptrl
 	BL output_string	; print the legend to putty
 	BL uart_disable
@@ -308,10 +410,57 @@ lab7:
 
 	BL gpio_init
 
+	MOV r10, #0x7000
+	MOVT r10, #0x2000
+
+	MOV r1, #0x00
+
+
+	MOV r0, #0x2323	; ascii for # four wide is a truck
+	MOVT r0, #0x2323
+	STR r0, [r10], #4
+	STRB r1, [r10], #1
+
+	MOV r0, #0x43	; ascii for C one wide is a car
+	STRB r0, [r10], #1
+	STRB r1, [r10], #1	; #0x20007005
+
+	MOV r0, #0x41	; ascii for A one wide is an alligator mouth
+	STRB r0, [r10], #1
+
+	MOV r0, #0x6161	; ascii for a five wide is an alligator back
+	MOVT r0, #0x6161
+	STR r0, [r10], #4
+	MOV r0, #0x61	; end of alligator back
+	STRB r0, [r10], #1
+	STRB r1, [r10], #1	; #0x20007007
+
+	MOV r0, #0x4C4C	; ascii for L six wide is a log
+	MOVT r0, #0x4C4C
+	STR r0, [r10], #4
+	MOV r0, #0x4C4C	; end of log
+	STRH r0, [r10], #2
+	STRB r1, [r10], #1	; #0x2000700e
+
+	MOV r0, #0x5454	; ascii for T two wide is a turtle
+	STRH r0, [r10], #2
+	STRB r1, [r10], #1	; #0x20007015
+
+	MOV r0, #0x4F	; ascii for O one wide is a lilypad
+	STRB r0, [r10], #1
+	STRB r1, [r10], #1	; #0x20007018
+
+
 gamestart:
 	LDR r4, ptr
 	BL output_string	; print the board to putty
 	MOV v4, #0
+	MOV v3, #60
+	MOV v7, #0
+
+	MOV r9, v3
+	BL inttoascii
+	BL character_newline
 
 	ADR r4, startprompt
 	BL output_string
@@ -356,8 +505,7 @@ gameover:
 
 	MOV r9, v4
 	BL inttoascii
-	BL character_newline
-	;need to print the score here
+	BL character_newline	; prints score
 
 	ADR r4, replayprompt
 	BL output_string	; prints prompt for playing again
