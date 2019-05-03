d56e16ca13368a5c834e7e5c94822e50a96d266b
Dean Kuhne <deankuhn@buffalo.edu>
2019-05-02 20:49:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,17 @@ extern int Timer0Handler(void);
 extern int Timer1Handler(void);
 extern int Uart0Handler(void);
 extern int PortAHandler(void);
+
+unsigned short lfsr = 0xACE1u;
+  unsigned bit;
+
+ unsigned rand()
+ {
+    bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+    lfsr =  (lfsr >> 1) | (bit << 15);
+    return lfsr >> 8;
+ }
+
 int main(void)
 {
     lab7();
